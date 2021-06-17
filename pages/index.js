59344aad298aca9e9b4f333dfacd586b67ba6af8
Rajan Davis <rajandavis@Rajans-MacBook-Pro.local>
2021-06-16 23:20:05 -0700

    Added an expression slider

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import {useState} from 'react'
 export default function Home(props) {
 
   // const [selectedPedal, setSelectedPedal] = useState('enzo')
+  const [expressionVal, setExpressionVal] = useState(0);
 
   return (
     <div className="container">
@@ -12,11 +13,26 @@ export default function Home(props) {
         <title>macrocosm</title>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
       </Head>
-      <div>
+      <div className="view-port">
+        <div className="pedal-selector">
+
+        </div>
         <MerisEnzoLayout
           sliderData={props.sliderData}
           midiObject={props.midiObject}
         />
+        <div className="expression-container">
+          <div className="expression">
+            <input type="range"
+                   value={expressionVal}
+                   min="0"
+                   max="127"
+                   onChange={(e)=>{ return setExpressionVal(e.target.value)}}/>
+            <label>
+              EXPRESSION
+            </label>
+          </div>
+        </div>
       </div>
     </div>
   )
