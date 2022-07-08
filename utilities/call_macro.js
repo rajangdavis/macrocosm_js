d5150489b0397acc4481e2d927efca0c80539f69
Rajan Davis <rajangdavis@gmail.com>
2022-07-07 22:26:27 -0400

    Starting to refactor more stuff...
    Fixed some macro calls, there were some bugs that I didn't realize
    Ended up grouping the calls as to limit the midi messaging to
    the bare minimum.
    
    Really trying to focus on styling... slowly working on it.

--- utilities/call_macro.js
+++ utilities/call_macro.js
@@ -11,7 +11,9 @@ export default async function callMacro(props) {
   let notSelectedPedals = macroData.pedals.filter((x) => x.showing == false);
 
   let macroMessageData = macroSelectedPedals.map((x) => {
-    let message = parseSysexToBinary(x.selectedPreset.message);
+    let message = x.selectedPreset.message
+      ? parseSysexToBinary(x.selectedPreset.message)
+      : false;
     return {
       name: x.name,
       channel: midiConfig[`${x.name}Channel`],
@@ -20,37 +22,40 @@ export default async function callMacro(props) {
   });
 
   let turnOffThesePedals = notSelectedPedals.map((x) => {
-    return { name: x.name, channel: midiConfig[`${x.name}Channel`] };
+    console.log("TURNING OFF: " + x.name);
+    return parseInt(midiConfig[`${x.name}Channel`]);
   });
 
   let deviceOutput = midiObject.outputs.filter((x) => {
     return x.name == midiConfig.output;
   })[0];
 
-  if (deviceOutput) {
-    const results = await Promise.all(
-      turnOffThesePedals
-        .map((x) => {
-          console.log("TURNING OFF PEDAL: " + x.name);
-          return deviceOutput.sendControlChange(14, 0, {
-            channels: parseInt(x.channel),
-          });
+  let buildPromises = () => {
+    return [
+      deviceOutput.sendControlChange(14, 0, {
+        channels: turnOffThesePedals,
+      }),
+      deviceOutput.sendControlChange(4, 0, {
+        channels: macroMessageData.map((x) => parseInt(x.channel)),
+      }),
+      deviceOutput.sendControlChange(14, 127, {
+        channels: macroMessageData.map((x) => parseInt(x.channel)),
+      }),
+    ].concat(
+      macroMessageData
+        .filter((x) => {
+          console.log("TURNING ON PRESET, RESETTING EXPRESSION: " + x.name);
+          if (x.message.data) console.log("SENDING SYSEX FOR: " + x.name);
+          return x.message.data;
         })
-        .concat(
-          macroMessageData.map((x) => {
-            console.log("TURNING ON PRESET, RESETTING EXPRESSION: " + x.name);
-            return [
-              deviceOutput.sendControlChange(4, 0, {
-                channels: parseInt(x.channel),
-              }),
-              deviceOutput.sendControlChange(14, 127, {
-                channels: parseInt(x.channel),
-              }),
-              deviceOutput.sendSysex(x.message.manufacturer, x.message.data),
-            ];
-          })
-        )
+        .map((x) => {
+          return deviceOutput.sendSysex(x.message.manufacturer, x.message.data);
+        })
     );
+  };
+
+  if (deviceOutput) {
+    const results = await Promise.all(buildPromises());
     console.log(results);
   }
 }
